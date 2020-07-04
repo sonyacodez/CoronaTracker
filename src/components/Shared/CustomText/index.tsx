a275804152ec8302fde4b58211f913c92d17ad98
Sonya Burlan <sonya.burlan@gmail.com>
2020-07-04 20:20:48 +0300

    created CustomText component

---
+++ src/components/Shared/CustomText/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useTheme } from '../../../hooks';
+import { IColors, IFontSizes } from '../../../types/styled';
+
+interface IProps {
+  text?: string;
+  bold?: boolean;
+  color?: keyof IColors;
+  size?: keyof IFontSizes;
+}
+
+const CustomText: React.FC<IProps> = ({ color = 'black', size = 's14', text, bold }) => {
+  const theme = useTheme();
+
+  return (
+    <span style={{ color: theme.colors[color], fontSize: theme.fontSizes[size] }}>{text}</span>
+  );
+};
+
+export default CustomText;
