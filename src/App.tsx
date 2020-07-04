a61938f1e5b826f59a59d87f51e3966c8bcb4b93
Sonya Burlan <sonya.burlan@gmail.com>
2020-07-04 23:57:56 +0300

    added and integrated i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import i18n from '../src/locale/i18n';
 import { defaultTheme } from './themes';
 import { GlobalStyle } from './utils/style';
+import { I18nextProvider } from 'react-i18next';
 import { ThemeProvider } from 'styled-components';
 import CustomText from './components/Shared/CustomText';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <CustomText text="Hello world!" color="purple1" size="s52" />
-    </ThemeProvider>
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <CustomText text="Hello world!" color="purple1" size="s52" />
+      </ThemeProvider>
+    </I18nextProvider>
   );
 }
 
 export default App;
+
+
