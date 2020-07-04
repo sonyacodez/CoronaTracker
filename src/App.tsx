c4d70ad1c9a20cb20939fa3c3d2c3e9f331d7cf8
Sonya Burlan <sonya.burlan@gmail.com>
2020-07-04 20:20:00 +0300

    wrapped App with ThemeProvider and GlobalStyle from styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { defaultTheme } from './themes';
+import { GlobalStyle } from './utils/style';
+import { ThemeProvider } from 'styled-components';
+import CustomText from './components/Shared/CustomText';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <CustomText text="Hello world!" color="purple1" size="s52" />
+    </ThemeProvider>
   );
 }
 
