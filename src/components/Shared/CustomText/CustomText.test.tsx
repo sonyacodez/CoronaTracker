efe62f39d594119fffab3ec26157af4eefe41ce7
Sonya Burlan <sonya.burlan@gmail.com>
2020-07-07 01:20:20 +0300

    completed CustomText tests

--- src/components/Shared/CustomText/CustomText.test.tsx
+++ src/components/Shared/CustomText/CustomText.test.tsx
@@ -5,26 +5,15 @@ import { RenderResult } from '@testing-library/react';
 
 describe('CustomText', () => {
   let CustomTextComponent: RenderResult;
-  const sizeProps = 's12';
-  const colorProps = 'black';
   const textProps = 'I am text';
 
   beforeEach(() => {
-    CustomTextComponent = render(
-      <CustomText text={textProps} />
-    )
+    CustomTextComponent = render(<CustomText text={textProps} />)
   });
 
   it('Render component with default styling', () => {
     const { container, getByText } = CustomTextComponent;
-      expect(container.firstChild).toMatchSnapshot();
-      expect(getByText(textProps)).toBeInTheDocument();
-  })
-  
-    it('Render component with default styling', () => {
-    const { container }= render(
-      <CustomText text={textProps} color={colorProps} size={sizeProps} />
-    )
-      expect(container.firstChild).
+    expect(container.firstChild).toMatchSnapshot();
+    expect(getByText(textProps)).toBeInTheDocument();
   })
 });
