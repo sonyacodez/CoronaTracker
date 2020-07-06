419339607cc7f4055b923374073d0b6e71c3d186
Sonya Burlan <sonya.burlan@gmail.com>
2020-07-07 01:20:56 +0300

    updated useTheme hook to come from styled components, not from local library

--- src/components/Shared/CustomText/index.tsx
+++ src/components/Shared/CustomText/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme } from '../../../hooks';
+import { useTheme } from 'styled-components';
 import { IColors, IFontSizes } from '../../../types/styled';
 
 interface IProps {
--- src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { DefaultTheme, ThemeContext } from 'styled-components/macro';
-
-const useTheme = (): DefaultTheme => {
-  const theme = useContext(ThemeContext);
-  return theme ?? {};
-};
-
-export default useTheme;
