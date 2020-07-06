832031f40b48050aa8ea1fe9aa2ca40cea0dcd81
Sonya Burlan <sonya.burlan@gmail.com>
2020-07-07 01:21:24 +0300

    added PlainFunction type to types folder

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,4 +1,6 @@
 // import React from 'react';
 // import { DefaultTheme } from 'styled-components';
 
+export type PlainFunction<T = any> = (params?: T) => any;
+
 export type JSXFunction<T = any> = (params?: T) => JSX.Element;
