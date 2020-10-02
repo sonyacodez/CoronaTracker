14ac5061a281ec728db26a93cff3996a8330d20f
Sonya Burlan <sonya.burlan@gmail.com>
2020-10-02 19:13:16 +0300

    added new translations

--- src/screens/Overview/Overview.tsx
+++ src/screens/Overview/Overview.tsx
@@ -2,28 +2,32 @@ import React, { FC } from 'react';
 import * as S from './Overview.style';
 import { useTranslation } from 'react-i18next';
 import StatMiniCard from '../../components/StatMiniCard/StatMiniCard';
+import AgeRangeGraph from '../../components/AgeRangeGraph/AgeRangeGraph';
 
 const Overview: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <S.CardWrapper>
-      <StatMiniCard
-        currentNumOfCases={123000}
-        previousNumOfCases={169793}
-        title={t('statMiniCards.active.title')}
-      />
-      <StatMiniCard
-        currentNumOfCases={169793}
-        previousNumOfCases={123000}
-        title={t('statMiniCards.death.title')}
-      />
-      <StatMiniCard
-        currentNumOfCases={169793}
-        previousNumOfCases={123000}
-        title={t('statMiniCards.recovered.title')}
-      />
-    </S.CardWrapper>
+    <div style={{ display: 'flex', margin: '20px' }}>
+      <S.CardWrapper>
+        <StatMiniCard
+          currentNumOfCases={123000}
+          previousNumOfCases={169793}
+          title={t('caseStatuses.active')}
+        />
+        <StatMiniCard
+          currentNumOfCases={169793}
+          previousNumOfCases={123000}
+          title={t('caseStatuses.death')}
+        />
+        <StatMiniCard
+          currentNumOfCases={169793}
+          previousNumOfCases={123000}
+          title={t('caseStatuses.recovered')}
+        />
+      </S.CardWrapper>
+      <AgeRangeGraph />
+    </div>
   );
 };
 
