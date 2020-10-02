171980858bfbbc5c395ea7a2b1a2e134b37481c6
Sonya Burlan <sonya.burlan@gmail.com>
2020-10-02 20:12:26 +0300

    updated Legend in AgeRangeGraph

--- src/components/AgeRangeGraph/AgeRangeGraph.style.ts
+++ src/components/AgeRangeGraph/AgeRangeGraph.style.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components/macro';
-import { IColors } from '../../types/styled';
 
 export const KeyContainer = styled.div`
   display: flex;
   align-items: center;
+  margin-right: 2rem;
 `;
 
 export const Container = styled.div`
@@ -17,21 +17,22 @@ export const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0px 5px 20px 0px ${({ theme }) => theme.colors.lightGray2};
 
-  & > div > ${KeyContainer}:first-of-type {
+  & > div > ${KeyContainer} {
     margin-top: 2rem;
-  }
-  & > div > ${KeyContainer}:last-of-type {
     margin-bottom: 2rem;
   }
 `;
 
+export const Legend = styled.div`
+  display: flex;
+`;
 
-export const KeyColorSquare = styled.div <{ keyColor: keyof IColors }>`
+export const KeyColorSquare = styled.div<{ keyColor: string }>`
   width: 1.5rem;
   height: 1.5rem;
   margin-right: 0.5rem;
   margin-bottom: 0.1rem;
   border-radius: 3.5px;
-  background-color: ${({ keyColor, theme: { colors } }) => colors[keyColor]};
+  background-color: ${({ keyColor }) => keyColor};
   box-shadow: 0px 3px 3px 0px ${({ theme: { colors } }) => colors.lightGray2};
 `;
--- src/components/AgeRangeGraph/AgeRangeGraph.tsx
+++ src/components/AgeRangeGraph/AgeRangeGraph.tsx
@@ -1,30 +1,31 @@
 import React, { FC } from 'react';
 import * as S from './AgeRangeGraph.style';
-import { IColors } from '../../types/styled';
+import { useTheme } from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import CustomText from '../Shared/CustomText/CustomText';
 
 interface IProps {}
 
 const AgeRangeGraph: FC<IProps> = () => {
+  const { colors } = useTheme();
   const { t } = useTranslation();
-  const legendKeyMapper: { [text: string]: keyof IColors } = {
-    [t('caseStatuses.active')]: 'teal',
-    [t('caseStatuses.recovered')]: 'green1',
-    [t('caseStatuses.death')]: 'red'
+  const legendKeyMapper: { [text: string]: string } = {
+    [t('caseStatuses.active')]: colors.teal,
+    [t('caseStatuses.death')]: colors.red,
+    [t('caseStatuses.recovered')]: colors.green1
   };
 
   return (
     <S.Container>
       <CustomText bold size="s18" text={t('ageRangeGraph.title')} />
-      <div>
+      <S.Legend>
         {Object.entries(legendKeyMapper).map(([text, color]) => (
           <S.KeyContainer>
             <S.KeyColorSquare keyColor={color} />
             <CustomText size="s14" text={text} />
           </S.KeyContainer>
         ))}
-      </div>
+      </S.Legend>
     </S.Container>
   );
 };
