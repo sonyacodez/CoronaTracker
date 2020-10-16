f9f6718b54361663ddaf22149cc8d61ef67d5035
Sonya Burlan <sonya.burlan@gmail.com>
2020-10-16 16:28:51 +0300

    changed App component to arrow

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,11 @@ import { I18nextProvider } from 'react-i18next';
 import { ThemeProvider } from 'styled-components';
 import Overview from './screens/Overview/Overview';
 
-function App() {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
-        <Overview />
-      </ThemeProvider>
-    </I18nextProvider>
-  );
-}
-
-export default App;
+export const App = () => (
+  <I18nextProvider i18n={i18n}>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Overview />
+    </ThemeProvider>
+  </I18nextProvider>
+);
